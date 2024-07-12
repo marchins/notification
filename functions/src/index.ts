117c0e894942d641203c5852c3cdb5d0ef783545
mconti7 <marco.conti@accenture.com>
2024-07-13 01:05:26 +0200

    extract method + avoid saving event if past

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -37,59 +37,23 @@ async function checkEventExists(name: string, date: Date): Promise<boolean> {
     return !querySnapshot.empty; // Returns true if the event exists
   } catch (error) {
     console.error("Error checking event existence:", error);
-    throw new functions.https.HttpsError("internal", "Error checking event existence");
+    throw new functions.https.HttpsError(
+      "internal",
+      "Error checking event existence"
+    );
   }
 }
 
 export const scrapeEvents = functions
   .region("europe-west1")
-  .pubsub
-  .schedule("every 24 hours")
+  .pubsub.schedule("every 24 hours")
   .onRun(async (context) => {
     try {
-      const {data} = await axios.get(url);
-      const $ = cheerio.load(data);
-
-      const events: Event[] = [];
-      const eventsPromises: Promise<void>[] = []; // Array to store Promises
-
-      $(".container")
-        .find(".row.row0.row-eq-height")
-        .each((i: any, element: any) => {
-          const name = $(element)
-            .find(".boxNota")
-            .find(".titolo.alignTextCenter div")
-            .text().trim();
-
-          const stringedDate = $(element)
-            .find(".boxNota")
-            .find(".titolo span.uppercase")
-            .text().trim();
-
-          const dateWithoutDay = stringedDate.replace(/^[a-zA-ZàèìòùÀÈÌÒÙ]+\s/, "");
-          console.log("1. Concerto: " + name + " " + dateWithoutDay);
-          const date = parse(dateWithoutDay, formatString, new Date(), {locale: it});
-
-          if (name && date) {
-            eventsPromises.push(new Promise((resolve, reject) => {
-              checkEventExists(name, date)
-                .then((eventExists) => {
-                  if (!eventExists) {
-                    console.log("2. Inserimento nuovo concerto " + name + " " + date);
-                    events.push({
-                      name,
-                      date,
-                      location,
-                    });
-                  } else {
-                    console.log("2. Concerto " + name + " già presente");
-                  }
-                  resolve(); // Resolve the promise for the current event
-                })
-                .catch(reject); // Reject the promise if there is an error
-            }));
-          }
-        });
+      const {
+        eventsPromises,
+        events,
+      }: { eventsPromises: Promise<void>[]; events: Event[] } =
+        await scrapeSanSiroConcerts();
 
       // Wait for all checkEventExists Promises to resolve
       await Promise.all(eventsPromises);
@@ -109,3 +73,66 @@ export const scrapeEvents = functions
       throw new functions.https.HttpsError("internal", "Error scraping events");
     }
   });
+
+/**
+ *
+ * @return {Object} events
+ */
+async function scrapeSanSiroConcerts() {
+  const {data} = await axios.get(url);
+  const $ = cheerio.load(data);
+
+  const events: Event[] = [];
+  const eventsPromises: Promise<void>[] = []; // Array to store Promises
+
+  $(".container")
+    .find(".row.row0.row-eq-height")
+    .each((i: any, element: any) => {
+      const name = $(element)
+        .find(".boxNota")
+        .find(".titolo.alignTextCenter div")
+        .text()
+        .trim();
+
+      const stringedDate = $(element)
+        .find(".boxNota")
+        .find(".titolo span.uppercase")
+        .text()
+        .trim();
+
+      const dateWithoutDay = stringedDate.replace(/^[a-zA-ZàèìòùÀÈÌÒÙ]+\s/, "");
+      console.log("1. Concerto: " + name + " " + dateWithoutDay);
+      const date = parse(dateWithoutDay, formatString, new Date(), {
+        locale: it,
+      });
+
+      if (name && date) {
+        eventsPromises.push(
+          new Promise((resolve, reject) => {
+            checkEventExists(name, date)
+              .then((eventExists) => {
+                if (!eventExists) {
+                  if (new Date() < new Date(date)) {
+                    console.log("Evento già passato");
+                  } else {
+                    console.log(
+                      "2. Inserimento nuovo concerto " + name + " " + date
+                    );
+                    events.push({
+                      name,
+                      date,
+                      location,
+                    });
+                  }
+                } else {
+                  console.log("2. Concerto " + name + " già presente");
+                }
+                resolve(); // Resolve the promise for the current event
+              })
+              .catch(reject); // Reject the promise if there is an error
+          })
+        );
+      }
+    });
+  return {eventsPromises, events};
+}
