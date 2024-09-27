88413c5c0a5261ea83f87fae0d64504171d6a7f8
marchins <m.conti89@gmail.com>
2024-09-27 02:38:10 +0200

    removed useless container

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -65,99 +65,99 @@ class ListTileWidget extends StatelessWidget {
           // TODO localization with properties?
           title: const Text('Upcoming events'),
         ),
-        body: Container(
-          child: StreamBuilder<QuerySnapshot>(
-          stream: firestoreService.getEvents(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              // TODO localization with properties?
-              return const Center(child: Text('No events available'));
-            } else if (snapshot.hasData) {
-              List<Event> eventsList = snapshot.data!.docs
-                  .map((doc) => Event.fromFirestore(doc))
-                  .toList();
-
-              eventsList.sort((a, b) => a.date.compareTo(b.date));
-              eventsList.removeWhere((element) => isBeforeToday(element.date));
-
-              if (eventsList.isEmpty) {
-                return const Card(
-                  child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text("Non ci sono eventi per adesso :)"),
-                      )),
-                );
-              }
-              
-              return ListView.builder(
-                itemCount: eventsList.length,
-                itemBuilder: (context, index) {
-                  Event event = eventsList[index];
-                  Timestamp timestamp = event.date;
-                  //String formattedDate = formatter.format(timestamp.toDate());
-                  DateTime eventDate = timestamp.toDate();
-                  int daysUntilEvent =
-                      eventDate.difference(DateTime.now()).inDays;
-
-                  var todayEventColor = Theme.of(context).brightness == Brightness.dark ? Color.fromARGB(80, 67, 109, 135) : Color.fromARGB(255, 249, 229, 131);
-
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    elevation: 4,
-                    color: daysUntilEvent == 0 ? todayEventColor : null,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            event.name,
-                            textAlign: TextAlign.center,
-                            // TODO replace const textstyle with theme
-                            style: const TextStyle(
-                              fontSize: 21,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            "${DateFormat(DateFormat.WEEKDAY).format(eventDate)} ${DateFormat('dd MMMM').format(eventDate)}",
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            "${event.location}",
-                            style: TextStyle(fontSize: 17),
-                          ),
-                          const SizedBox(height: 8),
-                          //TODO localization
-                          Text(
-                            daysUntilEvent > 0
-                                ? "Tra ${daysUntilEvent} giorni"
-                                : "Oggi",
-                            style: daysUntilEvent < 30 ? TextStyle(fontSize: 15, color: Colors.green) : null,
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
-            } else {
+        body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.getEvents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            // TODO localization with properties?
+            return const Center(child: Text('No events available'));
+          } else if (snapshot.hasData) {
+            List<Event> eventsList = snapshot.data!.docs
+                .map((doc) => Event.fromFirestore(doc))
+                .toList();
+        
+            eventsList.sort((a, b) => a.date.compareTo(b.date));
+            eventsList.removeWhere((element) => isBeforeToday(element.date));
+        
+            if (eventsList.isEmpty) {
               return const Card(
                 child: Padding(
                     padding: EdgeInsets.all(16.0),
                     child: Align(
                       alignment: Alignment.center,
-                      child: Text("Non ci sono eventi"),
+                      child: Text("Non ci sono eventi per adesso :)"),
                     )),
               );
             }
-          },
-        )));
+            
+            return ListView.builder(
+              itemCount: eventsList.length,
+              itemBuilder: (context, index) {
+                Event event = eventsList[index];
+                Timestamp timestamp = event.date;
+                //String formattedDate = formatter.format(timestamp.toDate());
+                DateTime eventDate = timestamp.toDate();
+                int daysUntilEvent =
+                    eventDate.difference(DateTime.now()).inDays;
+        
+                //TODO non camnbia automaticamente quando cambia mode, serve riavvio app. migliorare passando ai themes
+                var todayEventColor = Theme.of(context).brightness == Brightness.dark ? Color.fromARGB(80, 67, 109, 135) : Color.fromARGB(255, 249, 229, 131);
+        
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  elevation: 4,
+                  color: daysUntilEvent == 0 ? todayEventColor : null,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          event.name,
+                          textAlign: TextAlign.center,
+                          // TODO replace const textstyle with theme
+                          style: const TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          "${DateFormat(DateFormat.WEEKDAY).format(eventDate)} ${DateFormat('dd MMMM').format(eventDate)}",
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          "${event.location}",
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        const SizedBox(height: 8),
+                        //TODO localization
+                        Text(
+                          daysUntilEvent > 0
+                              ? "Tra ${daysUntilEvent} giorni"
+                              : "Oggi",
+                          style: daysUntilEvent < 30 ? TextStyle(fontSize: 15, color: Colors.green) : null,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Card(
+              child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("Non ci sono eventi"),
+                  )),
+            );
+          }
+        },
+                ));
   }
 }
